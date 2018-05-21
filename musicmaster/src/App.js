266d165304e715c4357e6d3b4aae4827cbed4fa7
DESKTOP-5AJDS5J\HoaBV <hoabuiyt@gmail.com>
2018-05-21 07:52:14 +0700

    update App.js
    init App and App.css

--- musicmaster/src/App.js
+++ musicmaster/src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 import './App.css';
 
 export class App extends Component {
+
+	constructor(props){
+		super(props);
+		this.state={
+			query:''
+		}
+	}
+
+	/* Hàm tìm kiếm nghệ sỹ*/
+	search(){
+		console.log('this.state',this.state);
+		const BASE_URL = 'https://api.spotify.com/v1/search?q=' + encodeURI(this.state.query) + '&type=artist&limit=1';
+
+		console.log('BASE_URL',BASE_URL);
+
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<div className="App-title">Music Master</div>
-				<div>
-					<input placeholder="nhap tu khoa" />
-					<button>Submit</button>
-				</div>
+
+				<FormGroup>
+					<InputGroup>
+						{/* Input key*/}
+						<FormControl
+							type="text"
+							placeholder="Nhap tu khoa"
+							value={this.state.query}
+							onChange={event=>this.setState({query:event.target.value})}
+							onKeyPress={event=> {
+								if(event.key === 'Enter'){
+									this.search();
+								}
+							}}
+						/>
+						{/* search button */}
+						<InputGroup.Addon onClick={()=>this.search()}>
+							<Glyphicon glyph="search"></Glyphicon>
+						</InputGroup.Addon>	
+					</InputGroup>									
+				</FormGroup>
+
 				<div className="Profile">
 					<div>Picture</div>
 					<div>Name</div>
